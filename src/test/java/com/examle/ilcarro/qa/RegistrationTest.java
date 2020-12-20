7758af39d1011041b589cf4b53e25dd0d800b70d
Yulia-94 <75741015+Yulia-94@users.noreply.github.com>
2020-12-20 17:30:40 +0300

    Added

--- src/test/java/com/examle/ilcarro/qa/RegistrationTest.java
+++ src/test/java/com/examle/ilcarro/qa/RegistrationTest.java
@@ -1,12 +1,14 @@
 package com.examle.ilcarro.qa;
 
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 
 public class RegistrationTest {
     WebDriver wd;
@@ -18,8 +20,29 @@ public class RegistrationTest {
     }
 
     @Test
-    public void testRegistration(){
+    public void testRegistration() throws InterruptedException {
 
+        wd.findElement(By.cssSelector("[href='/signup']")).click();
+       // wd.findElement(By.cssSelector("[href='/car']"));
+
+
+        wd.findElement(By.id("first_name")).click();
+        wd.findElement(By.id("first_name")).clear();
+        wd.findElement(By.id("first_name")).sendKeys("Yulia");
+        wd.findElement(By.id("second_name")).click();
+        wd.findElement(By.id("second_name")).clear();
+        wd.findElement(By.id("second_name")).sendKeys("Rosenblum");
+        wd.findElement(By.id("email")).click();
+        wd.findElement(By.id("email")).clear();
+        wd.findElement(By.id("email")).sendKeys("1234rosen@gmail.com");
+        wd.findElement(By.id("password")).click();
+        wd.findElement(By.id("password")).clear();
+        wd.findElement(By.id("password")).sendKeys("Trervgd123");
+        wd.findElement(By.id("check_policy")).click();
+        wd.findElement(By.className("yalla_yalla__1Jxk6")).click();
+
+        wd.findElement(By.name("address")).click();
+        Thread.sleep(3000);
     }
 
     @AfterMethod
