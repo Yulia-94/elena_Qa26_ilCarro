5ad35ce03e37ef3ed235bda2400354161550f54e
Yulia-94 <75741015+Yulia-94@users.noreply.github.com>
2020-12-24 16:42:50 +0300

    reg

--- src/test/java/com/examle/ilcarro/qa/RegistrationTest.java
+++ src/test/java/com/examle/ilcarro/qa/RegistrationTest.java
@@ -2,7 +2,6 @@ package com.examle.ilcarro.qa;
 
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -16,34 +15,39 @@ public class RegistrationTest {
     @BeforeMethod
     public void setUp(){
         wd = new ChromeDriver();
-        wd.get("https://ilcarro-dev-v1.firebaseapp.com/");
+        wd.navigate().to("https://ilcarro-dev-v1.firebaseapp.com/");
+        //wd.get("https://ilcarro-dev-v1.firebaseapp.com/");
     }
 
     @Test
     public void testRegistration() throws InterruptedException {
 
-        wd.findElement(By.cssSelector("[href='/signup']")).click();
-       // wd.findElement(By.cssSelector("[href='/car']"));
 
+        wd.findElement(By.cssSelector("[href='/signup]")).click();
 
-        wd.findElement(By.id("first_name")).click();
-        wd.findElement(By.id("first_name")).clear();
-        wd.findElement(By.id("first_name")).sendKeys("Yulia");
-        wd.findElement(By.id("second_name")).click();
-        wd.findElement(By.id("second_name")).clear();
-        wd.findElement(By.id("second_name")).sendKeys("Rosenblum");
-        wd.findElement(By.id("email")).click();
-        wd.findElement(By.id("email")).clear();
-        wd.findElement(By.id("email")).sendKeys("1234rosen@gmail.com");
-        wd.findElement(By.id("password")).click();
-        wd.findElement(By.id("password")).clear();
-        wd.findElement(By.id("password")).sendKeys("Trervgd123");
-        wd.findElement(By.id("check_policy")).click();
-        wd.findElement(By.className("yalla_yalla__1Jxk6")).click();
+        wd.findElement(By.cssSelector("#first_name")).click();
+        wd.findElement(By.cssSelector("#first_name")).clear();
+        wd.findElement(By.cssSelector("#first_name")).sendKeys("Yulia");
 
-       // wd.findElement(By.name("address")).click();
+        wd.findElement(By.cssSelector("#second_name")).click();
+        wd.findElement(By.cssSelector("#second_name")).clear();
+        wd.findElement(By.cssSelector("#second_name")).sendKeys("Rosenblum");
+
+        wd.findElement(By.cssSelector("#email")).click();
+        wd.findElement(By.cssSelector("#email")).clear();
+        wd.findElement(By.cssSelector("#email")).sendKeys("my.email" + System.currentTimeMillis() + "@gmail.com");
+
+        wd.findElement(By.cssSelector("#password")).click();
+        wd.findElement(By.cssSelector("#password")).clear();
+        wd.findElement(By.cssSelector("#password")).sendKeys("Ro123456");
+
+        wd.findElement(By.cssSelector("#check_policy")).click();
         Thread.sleep(3000);
+        wd.findElement(By.cssSelector("[type=submit]")).click();
+
     }
+
+
 
     @AfterMethod
     public void tearDown(){
