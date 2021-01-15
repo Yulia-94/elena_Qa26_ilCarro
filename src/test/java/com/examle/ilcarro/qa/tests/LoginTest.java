95c9f44edee86d2fcd0f13c3f6d8ed026211c4fa
Yulia-94 <75741015+Yulia-94@users.noreply.github.com>
2021-01-15 13:33:22 +0300

    dataProvider

--- src/test/java/com/examle/ilcarro/qa/tests/LoginTest.java
+++ src/test/java/com/examle/ilcarro/qa/tests/LoginTest.java
@@ -3,11 +3,15 @@ package com.examle.ilcarro.qa.tests;
 import com.examle.ilcarro.qa.model.User;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class LoginTest extends TestBase{
-
-
     @BeforeMethod
     public void ensurePreconditions(){
         /*if user logged in, logout*/
@@ -16,6 +20,66 @@ public class LoginTest extends TestBase{
         }
     }
 
+    @DataProvider
+    public Iterator<Object[]> validLogin(){
+        List<Object[]> list = new ArrayList<>();
+    list.add(new Object[]{"my.email1609079373951@gmail.com", "Ro123451"});
+    list.add(new Object[]{"my.email1608821574952@gmail.com", "iL12345678"});
+
+    return list.iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validLoginFromCsv() throws IOException {
+      List<Object[]> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/validLoginCsv.csv")));
+        String line = reader.readLine();
+        while(line != null){
+            String[] split = line.split(";");
+            list.add(new Object[]{new User().setEmail(split[0]).setPassword(split[1])});
+            line = reader.readLine();
+        }
+
+
+        return list.iterator();
+    }
+    @Test(dataProvider = "validLoginFromCsv")
+    public void testLoginDateProviderCsv(User user) throws InterruptedException {
+        logger.info("\n user name is" +user.getEmail() + "\n user password is " + user.getPassword());
+        app.getUser().clickOnLoginButton();
+        app.getUser().fillLoginForm(user);
+        app.getUser().pause(2000);
+        app.getUser().clickYallaButton();
+
+        Assert.assertTrue(app.getUser().isUserLoggedIn());
+        logger.info("Test Passed");
+
+        String screenshot = "src/test/screenshot-" + System.currentTimeMillis() + ".png";
+        app.getUser().takeScreenshot(screenshot);
+
+        logger.info("Created screenshot" + screenshot);
+
+    }
+
+
+
+    @Test(dataProvider = "validLogin")
+    public void testLoginDataProvider(String email, String password) throws InterruptedException {
+        logger.info("\n user name is " + email + "\n user password is " + password);
+        app.getUser().clickOnLoginButton();
+        app.getUser().fillLoginForm(new User().setEmail(email).setPassword(password));
+        app.getUser().pause(2000);
+        app.getUser().clickYallaButton();
+
+        Assert.assertTrue(app.getUser().isUserLoggedIn());
+        logger.info("Test Passed");
+
+        String screenshot = "src/test/screenshot-" + System.currentTimeMillis() + ".png";
+        app.getUser().takeScreenshot(screenshot);
+
+        logger.info("Created screenshot" + screenshot);
+
+    }
 
     @Test
     public void testLogin() throws InterruptedException {
@@ -28,12 +92,11 @@ public class LoginTest extends TestBase{
         Assert.assertTrue(app.getUser().isUserLoggedIn());
         logger.info("Test Passed");
 
-        String screenshot = " src/test/screenshot- " + System.currentTimeMillis() + ".png";
+        String screenshot = "src/test/screenshot-" + System.currentTimeMillis() + ".png";
         app.getUser().takeScreenshot(screenshot);
 
         logger.info("Created screenshot" + screenshot);
 
     }
-
 
 }
